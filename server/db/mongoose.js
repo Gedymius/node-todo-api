abe06423d0015d4a0cf06f63b990989c61a0db76
ASHENHEART\rzv <rzvgib001@gmail.com>
2018-03-03 16:50:52 +0400

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 // var { Schema } = mongoose;
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.PROD_MONGODB || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.PROD_MONGODB);
 
 module.exports = { mongoose };
