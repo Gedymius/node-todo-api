8423fc9927dc3e7ec64ac51fa37344c308b98fd9
Krzych <krzyg18@gmail.com>
2018-04-18 02:56:07 +0200

    Adding DETELE /todos/:id and tests for it

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -101,3 +101,30 @@ describe('GET /todos/:id', ()=> {
 	});
 	
 });
+
+describe('DELETE /todos/:id', ()=> {
+	it('should delete and return todo document', (done) =>{
+		request(app)
+			.delete(`/todos/${todos[1]._id.toHexString()}`)
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.text).toBe(todos[1].text);
+			})
+			.end(done);		
+	});
+	
+	it('should return 404 if todo not found', (done) => {
+		request(app)
+			.delete(`/todos/${new ObjectID().toHexString()}`)
+			.expect(404)
+			.end(done);
+	});
+	
+	it('should return 404 for non-object ids', (done) => {
+		request(app)
+			.delete('/todos/123')
+			.expect(404)
+			.end(done);
+	});
+	
+})
